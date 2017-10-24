989f623cc488feda8039e294e93ba6562eab5c4d
Jacob hilton <jacob.hilton@gmail.com>
2017-10-24 21:08:25 +0000

    safe play

--- evaluation.ml
+++ evaluation.ml
@@ -13,16 +13,25 @@ let eval = Fn.id
 (* mcts *)
 (* Bellman equation i.e. TD with depth plus discount factor *)
 (* TD *)
-(* look ahead 0: choose a legal board at random
-   look ahead 1: choose the best legal board (choose randomly between the best)
-   look ahead 2: for each legal board and each possible roll, choose the opponent's best board
-   look ahead 3: for each of the opponent's board choices and each possible roll, choose the best board *)
-(* let rec minimax ~look_ahead ~evaluation:_ player board roll = *)
-(*     match look_ahead with *)
-(*     | 0 -> 0.5 *)
-(*     | 1 -> evaluation player board *)
-
-let minimax t ~look_ahead:_ = t
+(* look ahead 0: constant
+   look ahead 1: evaluate once
+   look ahead 2: for each legal board and each possible roll, evaluate the opponent's boards
+   look ahead 3: for each of the opponent's boards and each possible roll, evaluate the boards *)
+let rec minimax t ~look_ahead player board =
+  match look_ahead with
+  | 0 -> 0.5
+  | 1 -> t player board
+  | _ -> 1. -. begin
+    List.map Roll.all_with_probabilities ~f:(fun (roll, probability) ->
+      Move.all_legal_turn_outcomes roll (Player.flip player) board
+      |> Set.to_list
+      |> List.map ~f:(fun new_board ->
+        (* have another player variable and use that to decide whether to max or min
+           just pass the same variable to minimax *)
+        minimax t ~look_ahead:(look_ahead - 1) (Player.flip player) new_board)
+      |> List.fold ~init:Float.min_value ~f:Float.max
+      |> Float.scale probability)
+    |> List.fold ~init:0. ~f:(+.) end
 
 let pip_count_ratio player board =
   let pip_count = Board.pip_count board ~player in
