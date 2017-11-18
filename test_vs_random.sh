d5a9302a257366463190edb6fe46b8133e30b470
Jacob hilton <jacob.hilton@gmail.com>
2017-11-18 23:14:59 +0000

    test vs random script

--- test_vs_random.sh
+++ test_vs_random.sh
@@ -5,5 +5,5 @@ GAMES="$4"
 for I in $(eval echo {$(($START+$STEP))..$END..$STEP}); do
     echo "After $(($I-$STEP)) games:"
     if [ "$I" -eq "$STEP" ]; then TO_LOAD=""; else TO_LOAD="ckpt/td_mod_$(($I-$STEP)).ckpt"; fi
-    _build/default/main.exe -X "(td (look_ahead 2) (hidden_layer_sizes (40)) (representation Modified) (ckpt_to_load ($TO_LOAD)))" -O '(pip_count_ratio (look_ahead 0))' -games "$GAMES"
+    _build/default/main.exe -X "(td (look_ahead 1) (hidden_layer_sizes (40)) (representation Modified) (ckpt_to_load ($TO_LOAD)))" -O '(pip_count_ratio (look_ahead 0))' -games "$GAMES"
 done >> "ckpt/test_td_mod_vs_random_$1_$2_$3_$4.txt"
